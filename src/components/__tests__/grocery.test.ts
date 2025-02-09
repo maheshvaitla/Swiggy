cf28d7fbada3263f21f305bf8814b4da89a005b7
maheshvaitla <maheshvaitla@gmail.com>
2025-02-09 09:01:10 +0530

    added 2nd test case

---
+++ src/components/__tests__/grocery.test.ts
@@ -0,0 +1,10 @@
+// import {render, screen } from "@testing-library/react"
+// import Grocery from "../Grocery/index"
+
+// test("should render gricery comoponent", () => {
+//     render(<Grocery />);
+
+//     const heading = screen.getByRole("h1");
+    
+//     expect(heading).toBeInTheDocument();
+// })
